6cdafaa82c01b599a1e061e676b055d48359593d
Chaithra-28 <77935519+Chaithra-28@users.noreply.github.com>
2025-03-02 17:12:52 +0530

    Create App.jsx

---
+++ Search Icon Project/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import HiddenSearchBar from './Components/HiddenSearchBar'
+
+const App = () => {
+
+  return (
+
+    <HiddenSearchBar />
+
+  )
+}
+
+export default App
