138dc71a37700742e4903992e4e3bc60cf4fe7a1
Chaithra-28 <77935519+Chaithra-28@users.noreply.github.com>
2025-03-07 15:28:57 +0530

    Create Products.jsx

---
+++ E - commerce Advance Filtering/src/Products/Products.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './Products.css'
+
+function Products({result}) {
+  return (
+
+    <>
+
+      <section className='card-container'>
+
+       {result} 
+
+      </section>
+      
+    </>
+
+  )
+}
+
+export default Products
